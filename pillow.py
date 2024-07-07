e4ebdcfb8c22c886ee3378ac23a9f6ea66d6ae10
BrianBeilby <brianbeilby98@gmail.com>
2024-07-07 15:45:01 -0700

    Update pillow.py

--- pillow.py
+++ pillow.py
@@ -1,3 +1,12 @@
 from PIL import Image
 im = Image.open("ascii-pineapple.jpg")
-print(im.size)
+height = 1000
+im.thumbnail((height, 200))
+pixels = list(im.getdata())
+pixel_matrix = [pixels[i:i+im.width] for i in range(0, len(pixels), im.width)]
+
+for x in range(len(pixel_matrix)):
+    for y in range(len(pixel_matrix[x])):
+        pixel = pixel_matrix[x][y]
+        brightness = (pixel[0] + pixel[1] + pixel[2]) / 3
+        print(brightness)
